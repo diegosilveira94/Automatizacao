4782e6b7651f2a4426c4039244cd3890d73d00ea
Diego Silveira <diegocilveira@gmail.com>
2025-04-03 14:40:09 -0300

    add locate position screen

--- automacao.py
+++ automacao.py
@@ -1,9 +1,15 @@
 import pyautogui as pa
 import time
-pa.PAUSE = 1
+pa.PAUSE = 2
 
 pa.press('win')
 pa.write('lightroom')
 pa.press('ENTER')
-pa.click(x=1626, y=335)
+time.sleep(5)
+pa.hotkey('ctrl','alt','1')
+pa.hotkey('ctrl','0')
+pos_Click1 = pa.locateOnScreen('filter_key_words.png')
+pa.click(pos_Click1)
 pa.write('1 - ')
+pos_Click2 = pa.locateOnScreen('number_1.png')
+pa.click(pos_Click2)
